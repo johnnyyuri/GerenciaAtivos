3f9fec56cd3a5f99e8f46aad640a00b9a6d4f82b
Gandour <eduardogandour@hotmail.com>
2020-09-08 11:30:23 -0300

    correcaotemplates

--- crud_ativos/admin.py
+++ crud_ativos/admin.py
@@ -2,5 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from.models import Ativo, Colaborador
 
-# Register your models here.
+admin.site.register(Ativo)
+admin.site.register(Colaborador)
